12ded284db8d14e57d055756f8c062cc4c4665b4
msbaleva <mimetyyy25@abv.bg>
2021-01-22 17:49:43 +0200

    css + refactoring

--- badges functionality/profile_view.js
+++
@@ -1,7 +0,0 @@
-﻿function main() {
-	document.getElementById("assign_badge_button").addEventListener("click", redirectToForm);
-}
-
-function redirectToForm() {
-	location.href = "badge_assignment_form.html";
-}
---
+++ script/profile_view.js
@@ -0,0 +1,7 @@
+﻿function main() {
+	document.getElementById("assign_badge_button").addEventListener("click", redirectToForm);
+}
+
+function redirectToForm() {
+	location.href = "badge_assignment_form.html";
+}
