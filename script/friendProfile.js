6c2a9d48c9fe959d3d15e34767c1b184834be472
RosinaGeorgieva <39170391+RosinaGeorgieva@users.noreply.github.com>
2021-01-25 17:08:08 +0200

    Update friendProfile.js

--- script/friendProfile.js
+++ script/friendProfile.js
@@ -15,32 +15,6 @@
 	
 }
 
-function displayBadges() {
-	var user = location.href.substring(location.href.lastIndexOf('/') + 1); //../profila-na-mimi
-
-	var data = new FormData();
-    data.append('user', user);
-
-    const settings = {
-		method: 'POST',
-        body: data
-    };
-
-    ajax('src/api.php/show-badges', settings, function (data) {
-		data.forEach(record => display(record));
-    }, function (error) {
-       // alert(error);
-    },
-    );
-}
-
-function display(record) {
-	document.getElementById("badgesPanel").appendChild(document.createElement('img')).src = "badge_icons/" + record.iconId + ".png";
-	document.getElementById("badgesPanel").innerHTML += "<br>";
-	document.getElementById("badgesPanel").innerHTML += "Значка &quot;" +  record.title + "&quot; <br>";
-	document.getElementById("badgesPanel").innerHTML += "Възложена от "+ record.assigningUser + "<br>";
-}
-
 function showBadgeSubmittionForm() {
 	cleanMessages();
 	document.getElementById("pageCover").style.display = "block";
