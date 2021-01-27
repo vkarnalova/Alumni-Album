427e2ec831028875fb300630ebfb82ec1ac71be4
unknown <vkarnalova@gmail.com>
2021-01-27 14:08:09 +0200

    fix show avatars bug

--- script/findUsers.js
+++ script/findUsers.js
@@ -18,24 +18,24 @@ function findUsers(username) {
         },
         );
     }
-    
+
 }
 
 function displayFoundUsers(users) {
-    
+
     let dropdown = document.getElementsByClassName("dropdownContent")[0];
     dropdown.style.display = "block";
     let ul = document.createElement("ul");
     dropdown.innerHTML = "";
-    
+
     for (let i = 0; i < users.length; i++) {
         console.log(users[i].username);
         let li = document.createElement("li");
         let a = document.createElement("a");
         a.classList.add("foundUsers");
         a.setAttribute("href", "friendProfile.html?" + users[i].username);
-        let avatar = document.createElement("img"); 
-        avatar.setAttribute("src", "avatars/" + users[i].username);
+        let avatar = document.createElement("img");
+        avatar.setAttribute("src", "avatars/" + users[i].username + ".png");
         avatar.setAttribute("onerror", "javascript:this.src='avatars/default.png'");
         avatar.setAttribute("width", "40");
         avatar.setAttribute("height", "40");
@@ -47,10 +47,10 @@ function displayFoundUsers(users) {
         div.appendChild(document.createElement("br"));
         div.appendChild(year);
         a.appendChild(div);
-        
+
         li.appendChild(a);
         ul.appendChild(li);
-        
+
     }
 
     let close = document.createElement("li");
@@ -61,15 +61,15 @@ function displayFoundUsers(users) {
     close.appendChild(closeIcon);
     closeIcon.setAttribute("onclick", "hideUsers()");
     ul.appendChild(close);
-   
+
     dropdown.appendChild(ul);
 }
 
 function hideUsers() {
     let dropdownContent = document.getElementsByClassName("dropdownContent")[0];
     document.getElementsByClassName("search")[0].value = "";
-    
+
     dropdownContent.style.display = "none";
     dropdownContent.innerHTML = "";
-    
+
 }
--- script/profileInformation.js
+++ script/profileInformation.js
@@ -4,15 +4,15 @@ function displayProfileInformation() {
 }
 
 function displayAvatar() {
-    var username = location.href.substring(location.href.lastIndexOf('/') + 1);
+    var username = location.href.substring(location.href.lastIndexOf('?') + 1);
     var avatar;
 
-    var avatarPNG = "avatars/"+username+".png";
-    if(fileExist(avatarPNG)) {
+    var avatarPNG = "avatars/" + username + ".png";
+    if (fileExist(avatarPNG)) {
         avatar = avatarPNG;
     } else {
-        var avatarJPG = "avatars/"+username+".jpg";
-        if(fileExist(avatarJPG)) {
+        var avatarJPG = "avatars/" + username + ".jpg";
+        if (fileExist(avatarJPG)) {
             avatar = avatarJPG;
         } else {
             avatar = "avatars/default.png";
@@ -26,7 +26,7 @@ function fileExist(urlToFile) {
     var xhr = new XMLHttpRequest();
     xhr.open('HEAD', urlToFile, false);
     xhr.send();
-     
+
     if (xhr.status == "404") {
         return false;
     } else {
@@ -38,14 +38,14 @@ function retrieveInformation() {
     var user = location.href.substring(location.href.lastIndexOf('/') + 1); //../profila-na-mimi
 
     const settings = {
-        method: 'GET', 
+        method: 'GET',
         headers: {
             'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8'
         },
     };
 
-    ajax('src/api.php/show-personal-information/'+user, settings, function (data) { 
-		fillInFieldsWithInformation(data);
+    ajax('src/api.php/show-personal-information/' + user, settings, function (data) {
+        fillInFieldsWithInformation(data);
     }, function (error) {
         document.getElementById("infoDetails").innerHTML = "Няма открита информация.";
     },
